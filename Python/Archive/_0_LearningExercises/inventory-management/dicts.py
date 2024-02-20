0867cf5a7b2ee59f4bdfa5162b884a867f326dd3
Tim Vorstenbosch <dark.hunter005@gmail.com>
2024-02-20 17:24:03 +0100

    added more learning exercises, 5 to go

---
+++ Python/Archive/_0_LearningExercises/inventory-management/dicts.py
@@ -0,0 +1,60 @@
+"""Functions to keep track and alter inventory."""
+
+import itertools as ito
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+
+    return {k: len(list(v)) for k,v in ito.groupby( sorted(items) )}
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+
+    inventory.update( {k: inventory.get(k,0) + v for k,v in create_inventory(items).items()} )
+    return inventory
+
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    inventory.update( {k : inventory.get(k,0) - v if inventory.get(k,0) - v >=0 else 0 for k,v in create_inventory(items).items() if k in inventory} )
+    return inventory 
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+
+    inventory.pop(item,None)
+    return inventory
+
+
+def list_inventory(inventory):
+    """Create a list containing all (item_name, item_count) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    return [(k,v) for k,v in inventory.items() if v > 0]
+
